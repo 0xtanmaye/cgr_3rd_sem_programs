97c5303f694ecfd13d8339ea7c36d503d1f8195d
0xtanmaye <106451224+0xtanmaye@users.noreply.github.com>
2022-12-22 09:02:46 +0530

    Add 4 and 8 connected Boundary fill

---
+++ 6_4_BD_F.C
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <conio.h>
+#include <graphics.h>
+#include <dos.h>
+
+void flood_fill(int x, int y, int bd_color, int fillcolor)
+{
+	if(getpixel(x, y)!=bd_color && getpixel(x, y)!=fl_color)
+	{
+		delay(10);
+		putpixel(x, y, fillcolor);
+		flood_fill(x+1, y, defaultcolor, YELLOW);
+		flood_fill(x-1, y, defaultcolor, GREEN);
+		flood_fill(x, y+1, defaultcolor, RED);
+		flood_fill(x, y-1, defaultcolor, WHITE);
+	}
+}
+
+void main()
+{
+	int gd=DETECT, gm;
+	clrscr();
+	initgraph(&gd, &gm, "C:\\TURBOC3\\BGI");
+
+	rectangle(50, 50, 100, 100);
+	flood_fill(51, 51, WHITE, BLUE);
+
+	getch();
+}
