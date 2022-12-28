d1dee496ad56f2c9c310c588a821c3171cfc08e5
0xtanmaye <106451224+0xtanmaye@users.noreply.github.com>
2022-12-29 04:13:06 +0530

    Fixed incorrect function names

--- 6_8_BD_F.C
+++ 6_8_BD_F.C
@@ -3,20 +3,20 @@
 #include <graphics.h>
 #include <dos.h>
 
-void flood_fill_8(int x, int y, int bd_color, int fl_color)
+void boundary_fill(int x, int y, int bd_color, int fl_color)
 {
 	if(getpixel(x, y)!=bd_color && getpixel(x, y)!=fl_color)
 	{
 		delay(5);
 		putpixel(x, y, fl_color);
-		flood_fill_8(x+1, y, bd_color, fl_color);
-		flood_fill_8(x, y+1, bd_color, fl_color);
-		flood_fill_8(x-1, y, bd_color, fl_color);
-		flood_fill_8(x, y-1, bd_color, fl_color);
-		flood_fill_8(x+1, y+1, bd_color, fl_color);
-		flood_fill_8(x-1, y-1, bd_color, fl_color);
-		flood_fill_8(x-1, y+1, bd_color, fl_color);
-		flood_fill_8(x+1, y-1, bd_color, fl_color);
+		boundary_fill(x+1, y, bd_color, fl_color);
+		boundary_fill(x, y+1, bd_color, fl_color);
+		boundary_fill(x-1, y, bd_color, fl_color);
+		boundary_fill(x, y-1, bd_color, fl_color);
+		boundary_fill(x+1, y+1, bd_color, fl_color);
+		boundary_fill(x-1, y-1, bd_color, fl_color);
+		boundary_fill(x-1, y+1, bd_color, fl_color);
+		boundary_fill(x+1, y-1, bd_color, fl_color);
 	}
 }
 
@@ -28,7 +28,7 @@ void main()
 
 	circle(50,50, 25);
 	//rectangle(50, 50, 100, 100);
-	flood_fill_8(51, 51, WHITE, GREEN);
+	boundary_fill(51, 51, WHITE, GREEN);
 
 	getch();
 	closegraph();
